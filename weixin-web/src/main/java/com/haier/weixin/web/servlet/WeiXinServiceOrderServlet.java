7c866e37849db0bdae4864da65018368a161a194
leixw0102 <0102lei@163.com>
2016-06-05 22:24:58 +0800

    add serviceOrderServlet

---
+++ weixin-web/src/main/java/com/haier/weixin/web/servlet/WeiXinServiceOrderServlet.java
@@ -0,0 +1,9 @@
+package com.haier.weixin.web.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by ehl on 2016/6/5.
+ */
+public class WeiXinServiceOrderServlet extends HttpServlet {
+}
