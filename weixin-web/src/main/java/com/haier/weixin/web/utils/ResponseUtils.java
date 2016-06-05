612d7fd29917337f9109eb21d2484e1f12a84480
leixw0102 <0102lei@163.com>
2016-06-05 22:40:55 +0800

    add servlet

---
+++ weixin-web/src/main/java/com/haier/weixin/web/utils/ResponseUtils.java
@@ -0,0 +1,62 @@
+package com.haier.weixin.web.utils;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by ehl on 2016/6/5.
+ */
+public class ResponseUtils {
+    private static Logger logger = LoggerFactory.getLogger(ResponseUtils.class);
+    public static void returnInfo( HttpServletResponse response, int httpStatusCode, String returnInfo) throws IOException {
+        response.setStatus(httpStatusCode);
+        if (returnInfo != null && returnInfo.trim().length() > 0) {
+            PrintWriter out = response.getWriter();
+            logger.info("返回终端数据：" + returnInfo);
+            out.print(returnInfo);
+            out.flush();
+            out.close();
+        } else {
+            logger.info("本次请求无返回数据！");
+        }
+    }
+
+    protected void returnInfo( HttpServletResponse response, int httpStatusCode) throws IOException {
+        response.setStatus(httpStatusCode);
+        logger.info(httpStatusCode + "本次请求无返回数据！");
+    }
+
+    protected void returnInfo( HttpServletResponse response, int httpStatusCode, JSONObject returnJson) throws IOException {
+        if (returnJson == null || returnJson.isEmpty()) {
+            returnInfo( response, httpStatusCode);
+        } else {
+            returnInfo( response, httpStatusCode, returnJson.toString());
+        }
+    }
+
+    protected void returnInfo( HttpServletResponse response, int httpStatusCode, JSONArray returnJsonArray) throws IOException {
+        logger.info("本次请求结果状态码：" + httpStatusCode);
+        if (returnJsonArray == null || returnJsonArray.isEmpty()) {
+            returnInfo( response, httpStatusCode);
+        } else {
+            returnInfo( response, httpStatusCode, returnJsonArray.toString());
+        }
+    }
+
+    protected void returnInfo(HttpServletResponse response ,JSONObject json) throws IOException{
+        returnInfo( response,json.getIntValue("code"),json.toJSONString());
+
+    }
+
+    protected void returnInfo(HttpServletResponse response ,JSONObject json,Integer code) throws IOException{
+        returnInfo( response,code,json.toJSONString());
+
+    }
+}
