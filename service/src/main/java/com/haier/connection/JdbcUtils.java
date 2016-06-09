73e080ce568c61d43f7e32d7ba9fc6972238ac2f
Administrator <limingbright@yahoo.com>
2016-06-09 23:18:38 +0800

    连接池改成druid,配置监控

--- service/src/main/java/com/haier/connection/JdbcUtils.java
+++ service/src/main/java/com/haier/connection/JdbcUtils.java
@@ -1,43 +1,43 @@
-package com.haier.connection;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-/**
- * Created by leixw
- * <p/>
- * Author: leixw
- * Date: 2014/10/31
- * Time: 15:28
- */
-public class JdbcUtils {
-    public static String getResultForSql(String sql) {
-        Connection connection = null;
-        PreparedStatement pst = null;
-        ResultSet set = null;
-        try {
-            connection = Connections.getConnection();
-            pst = connection.prepareStatement(sql);
-            set = pst.executeQuery();
-            if (set.next()) {
-                return set.getString(1);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (null != set) {
-                    set.close();
-                }
-                if (null != pst) {
-                    pst.close();
-                }
-                Connections.returnConnection(connection);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return "";
-    }
-}
+//package com.haier.connection;
+//
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//
+///**
+// * Created by leixw
+// * <p/>
+// * Author: leixw
+// * Date: 2014/10/31
+// * Time: 15:28
+// */
+//public class JdbcUtils {
+//    public static String getResultForSql(String sql) {
+//        Connection connection = null;
+//        PreparedStatement pst = null;
+//        ResultSet set = null;
+//        try {
+//            connection = Connections.getConnection();
+//            pst = connection.prepareStatement(sql);
+//            set = pst.executeQuery();
+//            if (set.next()) {
+//                return set.getString(1);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                if (null != set) {
+//                    set.close();
+//                }
+//                if (null != pst) {
+//                    pst.close();
+//                }
+//                Connections.returnConnection(connection);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return "";
+//    }
+//}
