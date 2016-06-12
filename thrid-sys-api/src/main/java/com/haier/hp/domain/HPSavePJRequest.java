9c00893b639d6a7cc9ffae85d3cf6b32ee2edb7f
Administrator <limingbright@yahoo.com>
2016-06-12 22:33:12 +0800

    提交代码

---
+++ thrid-sys-api/src/main/java/com/haier/hp/domain/HPSavePJRequest.java
@@ -0,0 +1,435 @@
+package com.haier.hp.domain;
+
+/**
+ * Created by Administrator on 2016/6/12.
+ */
+public class HPSavePJRequest {
+    private String wo_id;
+    private String netname;
+    private String engineer_check;
+    private String fee_check;
+    private String satis_check;
+    private String if_check;
+    private String unsatisfy_reason;
+    private String if_fee_std;
+    private String if_fee_std_exe;
+    private String email;
+    private String cus_remark;
+    private String speed_satis;
+    private String td_satis;
+    private String tec_satis;
+    private String qual_satis;
+    private String pro_ty;
+    private String tel_stais;
+    private String buy_stais;
+    private String use_stais;
+    private String install_satis;
+    private String guanh_satis;
+    private String spport_satis;
+    private String buy_qd;
+    private String award_type;
+    private String customer_ip;
+    private String user_mobile;
+    private String if_ycjh;
+    private String delivery_satis;
+    private String not_ycjh_reason;
+    private String if_s_a_tb;
+    private String paper_type;
+    private String client_type;
+    private String if_azfw_ycdw;
+    private String if_yysj_fhyq;
+    private String if_yysj_zsdd;
+    private String if_fwms_ss;
+    private String data_source;
+    private String if_leave_service_phone;
+    private String if_qs_timely;
+    private String if_self;
+
+    @Override
+    public String toString() {
+        return "HPSavePJRequest{" +
+                "wo_id='" + wo_id + '\'' +
+                ", netname='" + netname + '\'' +
+                ", engineer_check='" + engineer_check + '\'' +
+                ", fee_check='" + fee_check + '\'' +
+                ", satis_check='" + satis_check + '\'' +
+                ", if_check='" + if_check + '\'' +
+                ", unsatisfy_reason='" + unsatisfy_reason + '\'' +
+                ", if_fee_std='" + if_fee_std + '\'' +
+                ", if_fee_std_exe='" + if_fee_std_exe + '\'' +
+                ", email='" + email + '\'' +
+                ", cus_remark='" + cus_remark + '\'' +
+                ", speed_satis='" + speed_satis + '\'' +
+                ", td_satis='" + td_satis + '\'' +
+                ", tec_satis='" + tec_satis + '\'' +
+                ", qual_satis='" + qual_satis + '\'' +
+                ", pro_ty='" + pro_ty + '\'' +
+                ", tel_stais='" + tel_stais + '\'' +
+                ", buy_stais='" + buy_stais + '\'' +
+                ", use_stais='" + use_stais + '\'' +
+                ", install_satis='" + install_satis + '\'' +
+                ", guanh_satis='" + guanh_satis + '\'' +
+                ", spport_satis='" + spport_satis + '\'' +
+                ", buy_qd='" + buy_qd + '\'' +
+                ", award_type='" + award_type + '\'' +
+                ", customer_ip='" + customer_ip + '\'' +
+                ", user_mobile='" + user_mobile + '\'' +
+                ", if_ycjh='" + if_ycjh + '\'' +
+                ", delivery_satis='" + delivery_satis + '\'' +
+                ", not_ycjh_reason='" + not_ycjh_reason + '\'' +
+                ", if_s_a_tb='" + if_s_a_tb + '\'' +
+                ", paper_type='" + paper_type + '\'' +
+                ", client_type='" + client_type + '\'' +
+                ", if_azfw_ycdw='" + if_azfw_ycdw + '\'' +
+                ", if_yysj_fhyq='" + if_yysj_fhyq + '\'' +
+                ", if_yysj_zsdd='" + if_yysj_zsdd + '\'' +
+                ", if_fwms_ss='" + if_fwms_ss + '\'' +
+                ", data_source='" + data_source + '\'' +
+                ", if_leave_service_phone='" + if_leave_service_phone + '\'' +
+                ", if_qs_timely='" + if_qs_timely + '\'' +
+                ", if_self='" + if_self + '\'' +
+                ", tel_no='" + tel_no + '\'' +
+                ", award_code='" + award_code + '\'' +
+                '}';
+    }
+
+    private String tel_no;
+
+    public String getWo_id() {
+        return wo_id;
+    }
+
+    public void setWo_id(String wo_id) {
+        this.wo_id = wo_id;
+    }
+
+    public String getNetname() {
+        return netname;
+    }
+
+    public void setNetname(String netname) {
+        this.netname = netname;
+    }
+
+    public String getEngineer_check() {
+        return engineer_check;
+    }
+
+    public void setEngineer_check(String engineer_check) {
+        this.engineer_check = engineer_check;
+    }
+
+    public String getFee_check() {
+        return fee_check;
+    }
+
+    public void setFee_check(String fee_check) {
+        this.fee_check = fee_check;
+    }
+
+    public String getSatis_check() {
+        return satis_check;
+    }
+
+    public void setSatis_check(String satis_check) {
+        this.satis_check = satis_check;
+    }
+
+    public String getIf_check() {
+        return if_check;
+    }
+
+    public void setIf_check(String if_check) {
+        this.if_check = if_check;
+    }
+
+    public String getUnsatisfy_reason() {
+        return unsatisfy_reason;
+    }
+
+    public void setUnsatisfy_reason(String unsatisfy_reason) {
+        this.unsatisfy_reason = unsatisfy_reason;
+    }
+
+    public String getIf_fee_std() {
+        return if_fee_std;
+    }
+
+    public void setIf_fee_std(String if_fee_std) {
+        this.if_fee_std = if_fee_std;
+    }
+
+    public String getIf_fee_std_exe() {
+        return if_fee_std_exe;
+    }
+
+    public void setIf_fee_std_exe(String if_fee_std_exe) {
+        this.if_fee_std_exe = if_fee_std_exe;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCus_remark() {
+        return cus_remark;
+    }
+
+    public void setCus_remark(String cus_remark) {
+        this.cus_remark = cus_remark;
+    }
+
+    public String getSpeed_satis() {
+        return speed_satis;
+    }
+
+    public void setSpeed_satis(String speed_satis) {
+        this.speed_satis = speed_satis;
+    }
+
+    public String getTd_satis() {
+        return td_satis;
+    }
+
+    public void setTd_satis(String td_satis) {
+        this.td_satis = td_satis;
+    }
+
+    public String getTec_satis() {
+        return tec_satis;
+    }
+
+    public void setTec_satis(String tec_satis) {
+        this.tec_satis = tec_satis;
+    }
+
+    public String getQual_satis() {
+        return qual_satis;
+    }
+
+    public void setQual_satis(String qual_satis) {
+        this.qual_satis = qual_satis;
+    }
+
+    public String getPro_ty() {
+        return pro_ty;
+    }
+
+    public void setPro_ty(String pro_ty) {
+        this.pro_ty = pro_ty;
+    }
+
+    public String getTel_stais() {
+        return tel_stais;
+    }
+
+    public void setTel_stais(String tel_stais) {
+        this.tel_stais = tel_stais;
+    }
+
+    public String getBuy_stais() {
+        return buy_stais;
+    }
+
+    public void setBuy_stais(String buy_stais) {
+        this.buy_stais = buy_stais;
+    }
+
+    public String getUse_stais() {
+        return use_stais;
+    }
+
+    public void setUse_stais(String use_stais) {
+        this.use_stais = use_stais;
+    }
+
+    public String getInstall_satis() {
+        return install_satis;
+    }
+
+    public void setInstall_satis(String install_satis) {
+        this.install_satis = install_satis;
+    }
+
+    public String getGuanh_satis() {
+        return guanh_satis;
+    }
+
+    public void setGuanh_satis(String guanh_satis) {
+        this.guanh_satis = guanh_satis;
+    }
+
+    public String getSpport_satis() {
+        return spport_satis;
+    }
+
+    public void setSpport_satis(String spport_satis) {
+        this.spport_satis = spport_satis;
+    }
+
+    public String getBuy_qd() {
+        return buy_qd;
+    }
+
+    public void setBuy_qd(String buy_qd) {
+        this.buy_qd = buy_qd;
+    }
+
+    public String getAward_type() {
+        return award_type;
+    }
+
+    public void setAward_type(String award_type) {
+        this.award_type = award_type;
+    }
+
+    public String getCustomer_ip() {
+        return customer_ip;
+    }
+
+    public void setCustomer_ip(String customer_ip) {
+        this.customer_ip = customer_ip;
+    }
+
+    public String getUser_mobile() {
+        return user_mobile;
+    }
+
+    public void setUser_mobile(String user_mobile) {
+        this.user_mobile = user_mobile;
+    }
+
+    public String getIf_ycjh() {
+        return if_ycjh;
+    }
+
+    public void setIf_ycjh(String if_ycjh) {
+        this.if_ycjh = if_ycjh;
+    }
+
+    public String getDelivery_satis() {
+        return delivery_satis;
+    }
+
+    public void setDelivery_satis(String delivery_satis) {
+        this.delivery_satis = delivery_satis;
+    }
+
+    public String getNot_ycjh_reason() {
+        return not_ycjh_reason;
+    }
+
+    public void setNot_ycjh_reason(String not_ycjh_reason) {
+        this.not_ycjh_reason = not_ycjh_reason;
+    }
+
+    public String getIf_s_a_tb() {
+        return if_s_a_tb;
+    }
+
+    public void setIf_s_a_tb(String if_s_a_tb) {
+        this.if_s_a_tb = if_s_a_tb;
+    }
+
+    public String getPaper_type() {
+        return paper_type;
+    }
+
+    public void setPaper_type(String paper_type) {
+        this.paper_type = paper_type;
+    }
+
+    public String getClient_type() {
+        return client_type;
+    }
+
+    public void setClient_type(String client_type) {
+        this.client_type = client_type;
+    }
+
+    public String getIf_azfw_ycdw() {
+        return if_azfw_ycdw;
+    }
+
+    public void setIf_azfw_ycdw(String if_azfw_ycdw) {
+        this.if_azfw_ycdw = if_azfw_ycdw;
+    }
+
+    public String getIf_yysj_fhyq() {
+        return if_yysj_fhyq;
+    }
+
+    public void setIf_yysj_fhyq(String if_yysj_fhyq) {
+        this.if_yysj_fhyq = if_yysj_fhyq;
+    }
+
+    public String getIf_yysj_zsdd() {
+        return if_yysj_zsdd;
+    }
+
+    public void setIf_yysj_zsdd(String if_yysj_zsdd) {
+        this.if_yysj_zsdd = if_yysj_zsdd;
+    }
+
+    public String getIf_fwms_ss() {
+        return if_fwms_ss;
+    }
+
+    public void setIf_fwms_ss(String if_fwms_ss) {
+        this.if_fwms_ss = if_fwms_ss;
+    }
+
+    public String getData_source() {
+        return data_source;
+    }
+
+    public void setData_source(String data_source) {
+        this.data_source = data_source;
+    }
+
+    public String getIf_leave_service_phone() {
+        return if_leave_service_phone;
+    }
+
+    public void setIf_leave_service_phone(String if_leave_service_phone) {
+        this.if_leave_service_phone = if_leave_service_phone;
+    }
+
+    public String getIf_qs_timely() {
+        return if_qs_timely;
+    }
+
+    public void setIf_qs_timely(String if_qs_timely) {
+        this.if_qs_timely = if_qs_timely;
+    }
+
+    public String getIf_self() {
+        return if_self;
+    }
+
+    public void setIf_self(String if_self) {
+        this.if_self = if_self;
+    }
+
+    public String getTel_no() {
+        return tel_no;
+    }
+
+    public void setTel_no(String tel_no) {
+        this.tel_no = tel_no;
+    }
+
+    public String getAward_code() {
+        return award_code;
+    }
+
+    public void setAward_code(String award_code) {
+        this.award_code = award_code;
+    }
+
+    private String award_code;
+}
